6272d4f8769e45788217f80c8bb3ccce4d5df843
Yara <yara.gordillo@gmail.com>
2022-11-23 09:53:33 +0100

    develop hero portfolio

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,32 @@ function App() {
     <div className='App'>
       <main>
         <section className='hero'>
-          <h1 className='hero__title'>Hi, I'm Yara</h1>
+          <article className='hero__title'>
+            <h1 className='hero__welcome'>Hi, I'm Yara</h1>
+            <p className='hero__edition'>Front-End Edition</p>
+          </article>
           <nav>
             <ul className='hero__menu'>
-              <li>New Game</li>
-              <li>Continue</li>
-              <li>Load Game</li>
-              <li>Options</li>
+              <li className='hero__list__item'>
+                <a className='hero__link--newGame' href=''>
+                  New Game
+                </a>
+              </li>
+              <li className='hero__list__item '>
+                <a className='hero__link--continue' href=''>
+                  Continue
+                </a>
+              </li>
+              <li className='hero__list__item'>
+                <a className='hero__link--loadGame' href=''>
+                  Load Game
+                </a>
+              </li>
+              <li className='hero__list__item '>
+                <a className='hero__link--options' href=''>
+                  Options
+                </a>
+              </li>
             </ul>
           </nav>
         </section>
